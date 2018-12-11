1f60a02cd31d5b3fc47ca656699670d70b191538
gang <553985156@qq.com>
2018-12-10 17:12:00 -0800

    when system startpu,get kernel and ramdisk from tftp or nfs

--- file-gen/only_qflash_BOOT_BIN/makeboot.bat
+++ file-gen/only_qflash_BOOT_BIN/makeboot.bat
@@ -1,5 +1,6 @@
 
-bootgen -image  OUTPUT.bif -o i BOOT.BIN -w on
+bootgen -image  OUTPUT.bif -o i BOOT.bin -w on
+::
 bootgen -image  OUTPUT.bif -o i BOOT.mcs -w on
 
 PAUSE
--- python-script/boot_cmd.py
+++ python-script/boot_cmd.py
@@ -9,6 +9,14 @@ def message(str):
     crt.Screen.Print(str)
     pass
     
+def wait_string(str,timeout_s):
+    if (crt.Screen.WaitForString(str, timeout_s) != True):
+        crt.Dialog.MessageBox("wait string timeout!!")
+        return 0
+    sleep(500)
+    return 1
+
+    
 def SEND_CMD(str):
     crt.Screen.Send(str)
     crt.Screen.Send("\r")
@@ -18,7 +26,7 @@ def sleep(ms):
     crt.Sleep(ms)
     
     
-def boot():
+def sd_boot():
     """
     "sdboot=if mmcinfo; then " \
             "run uenvboot; " \
@@ -30,23 +38,61 @@ def boot():
     """
     SEND_CMD("mmcinfo")
     #SEND_CMD("run uenvboot")
-    SEND_CMD("load mmc 0 0x2080000 uImage && ")
+    SEND_CMD("load mmc 0 ${kernel_load_address} ${kernel_image} && ")
     sleep(1000)
-    SEND_CMD("load mmc 0 0x2000000 devicetree.dtb && ")
+    SEND_CMD("load mmc 0 ${devicetree_load_address} ${devicetree_image} && ")
     sleep(1000)
     SEND_CMD("load mmc 0 0x4000000 uramdisk.image.gz && ")
     sleep(1000)
     SEND_CMD("bootm 0x2080000 0x4000000 0x2000000")
-    #SEND_CMD("aaaaaa")
 
+    
+    
+def tftp_boot():
+    SEND_CMD("setenv ipaddr 192.168.68.100")#zynq board ip
+    SEND_CMD("setenv serverip 192.168.68.200") #pc ip
+    
+    SEND_CMD("tftpboot ${devicetree_load_address} ${devicetree_image}")
+    wait_string("Zynq>",5)
+    
+    SEND_CMD("tftpboot ${kernel_load_address} ${kernel_image}")
+    wait_string("Zynq>",20)
+    
+    SEND_CMD("tftpboot ${ramdisk_load_address} ${ramdisk_image}")
+    wait_string("Zynq>",30)
+    SEND_CMD("bootm ${kernel_load_address} ${ramdisk_load_address} ${devicetree_load_address}")
+
+    #SEND_CMD("aaaaaa")
+    
+def nfs_boot():
+    SEND_CMD("setenv ipaddr 192.168.68.100")#zynq board ip
+    SEND_CMD("setenv serverip 192.168.68.200") #pc ip
+    #SEND_CMD("setenv ipaddr 172.16.100.100")#zynq board ip
+    #SEND_CMD("setenv serverip 172.16.100.10") #pc ip
+    
+    
+    SEND_CMD("nfs ${devicetree_load_address} 192.168.68.200:/nfs/${devicetree_image}")
+    wait_string("Zynq>",20)
+    
+    sleep(500)
+
+    SEND_CMD("nfs ${kernel_load_address} 192.168.68.200:/nfs/${kernel_image}")
+    wait_string("Zynq>",20)
+    sleep(500)
+    
+    SEND_CMD("nfs ${ramdisk_load_address} 192.168.68.200:/nfs/${ramdisk_image}")
+    wait_string("Zynq>",20)
+    #SEND_CMD("bootm ${kernel_load_address} ${ramdisk_load_address} ${devicetree_load_address}")
+    
 
         
 def run():
     pass
 
 def main():
-    boot()
-    
+    #sd_boot()
+    #tftp_boot()
+    nfs_boot()    
     
 main()
 
